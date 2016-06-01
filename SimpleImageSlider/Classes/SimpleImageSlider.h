82a31f7e0ae1d05a64843b8be291468800a60f73
Christian Hatch <christianhatch@gmail.com>
2016-06-01 13:11:19 -0400

    Fixed documentation.

--- SimpleImageSlider/Classes/SimpleImageSlider.h
+++ SimpleImageSlider/Classes/SimpleImageSlider.h
@@ -13,10 +13,10 @@
 /**
  Designated Initializer for images!
  
- @param frame  The frame of the ImageSlider.
- @param images The images to be set on the ImageSlider
+ @param frame  The frame of the SimpleImageSlider.
+ @param images The images to be set on the SimpleImageSlider
  
- @return A new intialized ImageSlider
+ @return A new intialized SimpleImageSlider
  */
 + (nonnull instancetype)imageSliderWithFrame:(CGRect)frame
                                       images:(nullable NSArray<UIImage *> *)images;
@@ -24,21 +24,21 @@
 /**
  Designated Initializer for image URLs!
  
- @param frame  The frame of the ImageSlider.
- @param images The urls of the images to be set on the ImageSlider
+ @param frame  The frame of the SimpleImageSlider.
+ @param images The urls of the images to be set on the SimpleImageSlider
  
- @return A new intialized ImageSlider
+ @return A new intialized SimpleImageSlider
  */
 + (nonnull instancetype)imageSliderWithFrame:(CGRect)frame
                                    imageURLs:(nullable NSArray<NSURL *> *)imageURLs;
 
 /**
- An array of imageURLs used to populate the image scroller. Nil if the ImageSlider was created with images.
+ An array of imageURLs used to populate the image scroller. Nil if the SimpleImageSlider was created with images.
  */
 @property (nonatomic, copy, nullable) NSArray<NSURL *> *imageURLs;
 
 /**
- An array of images used to populate the image scroller. Nil if the ImageSlider was created with imageURLs.
+ An array of images used to populate the image scroller. Nil if the SimpleImageSlider was created with imageURLs.
  */
 @property (nonatomic, copy, nullable) NSArray<UIImage *> *images;
 
