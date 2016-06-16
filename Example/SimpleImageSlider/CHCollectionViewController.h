ebe4b37be71b9b778726094c1e8839724ba0bd09
Christian Hatch <christianhatch@gmail.com>
2016-06-16 14:58:26 -0400

    Added collectionview example.

---
+++ Example/SimpleImageSlider/CHCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHCollectionViewController.h
+//  SimpleImageSlider
+//
+//  Created by Christian Hatch on 6/16/16.
+//  Copyright © 2016 Christian Hatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHCollectionViewController : UICollectionViewController
+
+@end
---
+++ Example/SimpleImageSlider/CHCollectionViewController.m
@@ -0,0 +1,102 @@
+//
+//  CHCollectionViewController.m
+//  SimpleImageSlider
+//
+//  Created by Christian Hatch on 6/16/16.
+//  Copyright © 2016 Christian Hatch. All rights reserved.
+//
+
+#import "CHCollectionViewController.h"
+#import <SimpleImageSlider/SimpleImageSlider.h>
+
+@interface CHCollectionViewController () <UIScrollViewDelegate>
+@property (nonatomic, strong) SimpleImageSlider *header;
+@end
+
+@implementation CHCollectionViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    
+    [self setupHeader];
+}
+
+- (void)setupHeader {
+    NSArray *images = @[[UIImage imageNamed:@"image1"],
+                        [UIImage imageNamed:@"image2"],
+                        [UIImage imageNamed:@"image3"],
+                        [UIImage imageNamed:@"image4"],
+                        [UIImage imageNamed:@"image5"]];
+    
+    self.header = [SimpleImageSlider imageSliderWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 300) images:images];
+    [self.header addParallaxToScrollView:self.collectionView];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self.header scrollViewScrolled:scrollView];
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 200;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    for (UIView *view in cell.contentView.subviews) {
+        [view removeFromSuperview];
+    }
+    cell.backgroundColor = [UIColor lightGrayColor];
+
+    UILabel *label = [[UILabel alloc] initWithFrame:cell.bounds];
+    label.text = [NSString stringWithFormat:@"%li, %li", (long)indexPath.section, indexPath.row];
+    [cell.contentView addSubview:label];
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
